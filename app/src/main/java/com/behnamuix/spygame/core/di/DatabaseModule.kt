cb38f5636b78d6dffb8613cb2493dcf92b521996
Ali mahjoob <behnamuiux@gmail.com>
2026-09-09 16:57:19 +0330

    add db to dagger-hilt

--- app/src/main/java/com/behnamuix/spygame/core/di/DatabaseModule.kt
+++ app/src/main/java/com/behnamuix/spygame/core/di/DatabaseModule.kt
@@ -1,15 +1,30 @@
 package com.behnamuix.spygame.core.di
 
+import android.content.Context
+import androidx.room.Room
+import com.behnamuix.spygame.core.database.SpyDatabase
+import com.behnamuix.spygame.data.local.db.dao.KeywordDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DatabaseModule{
+object DatabaseModule {
     @Provides
     @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): SpyDatabase =
+        Room.databaseBuilder(
+            context,
+            SpyDatabase::class.java,
+            "spy.db"
+        ).build()
+
+    @Provides
+    fun provideKeywordDao(database: SpyDatabase): KeywordDao =
+        database.keyWordDao()
 
 }
