ad7e21bed5513eb7b29141a786d5d0a7ace4956c
Ali mahjoob <behnamuiux@gmail.com>
2026-09-16 12:16:02 +0330

    fix bug empty txtfield after add to db

--- app/src/main/java/com/behnamuix/spygame/feature/configword/presentation/screen/WordManagerSc.kt
+++ app/src/main/java/com/behnamuix/spygame/feature/configword/presentation/screen/WordManagerSc.kt
@@ -148,6 +148,7 @@ fun WordManagerSc(
                                                 keyWordVm.onAction(
                                                     UiAction.AddKeyword(KeyWord(word = uiState.word))
                                                 )
+                                                uiState.word = ""
                                             }
 
                                         },
